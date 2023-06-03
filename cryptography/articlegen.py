645577ad792ca5e246b29978ddefd4c1d53d84db
phthallo <84078890+phthallo@users.noreply.github.com>
2023-06-03 22:17:48 +0930

    Added Pig Latin + minor interface changes
    Pig Latin is now an option as a "cipher" and the terminal interactions have been neatened up.

--- cryptography/articlegen.py
+++ cryptography/articlegen.py
@@ -1,12 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import re 
-from ciphers import caesar
+from ciphers import caesar, piglatin
+import random
 
 done = False
 score = 0
 article_title = ""
 article_summary = ""
+ciphers = [caesar, piglatin]
+class color:
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
 def articleGen():
     arti = requests.get("https://en.wikipedia.org/wiki/Special:Random")
     soup = BeautifulSoup(arti.content, 'html.parser')
@@ -16,20 +23,36 @@ def articleGen():
         soup.b.unwrap()
     except:
         articleGen()
-    arti_summ = soup.find('p')
+    arti_summ = soup.find('p') #extract test that is contained with the <p> tags
     arti_summ = arti_summ.text
-    arti_summ = re.sub(r'\[.*?\]+', '', arti_summ)
+    arti_summ = re.sub(r'\[.*?\]+', '', arti_summ) #remove linked references to other articles [[like so]]
     return arti_title, arti_summ
+
+print(f"""   _    _ _   _ _____ _   _____________ ___________ _____ ___ 
+  | |  | | | | |  _  | | / |_   _| ___ |  ___|  _  |_   _/ _ \ 
+  | |  | | |_| | | | | |/ /  | | | |_/ | |__ | | | | | |/ /_\ \ 
+  | |/\| |  _  | | | |    \  | | |  __/|  __|| | | | | ||  _  | 
+  \  /\  | | | \ \_/ | |\  \_| |_| |   | |___| |/ / _| || | | | 
+   \/  \/\_| |_/\___/\_| \_/\___/\_|   \____/|___/  \___\_| |_/
+  ═════════════════════════════════════════════════════════════
+  The aim of the game is to guess the name of a random encoded Wikipedia article.
+  For ones that are encoded using ciphers {color.BOLD}cipherhelp.py{color.END} and {color.BOLD}commonletters.py{color.END} exist 
+  to help with manual deciphering. Type "quit" to end the game.
+""")
 
 while not done:
     article = articleGen()
     while article[1] == "\n": #sometimes the scraping fails and arti_summ only contains a new line. if this happens, find another new article
         article = articleGen()
-    print(caesar(article[1]))
-    print("Article name?")
+    print("\n"+random.choice(ciphers)(article[1])) #selects a random cipher and calls it using the summary 
+    print(f"{color.BOLD}\n// Article name?{color.END}")
     answer = input("> ")
-    if answer == str(article[0]):
+    if answer.lower() == str(article[0]).lower(): # case insensitive
         score += 1 
-        print(f"Correct! Your score is now {score}.")
+        print(f"// Correct! Your score is now {score}.")
     elif answer == "quit":
+        print(f"// Game ended. Your score was {score}.")
         done = True
+    else: 
+        print(f"// The answer was '{str(article[0])}'!")
+
--- cryptography/ciphers.py
+++ cryptography/ciphers.py
@@ -1,13 +1,28 @@
 import random
-from string import ascii_lowercase, ascii_uppercase
-alphabets = (ascii_lowercase, ascii_uppercase) #to make it easier for players and similar to the original, numbers and punctuation are not shifted.
+from string import ascii_lowercase, ascii_uppercase, punctuation
+caes_alphabets = [ascii_lowercase,ascii_uppercase] #to make it easier for players and similar to the original, numbers and punctuation are not shifted. works for ones that require shifts.
+lowercase = ' '.join(ascii_lowercase).split() 
+uppercase = ' '.join(ascii_uppercase).split()
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 def caesar(summary):
     step = random.randint(1,25) #step is randomly generated
     def shift(alphabet):
         return (alphabet[step:]) + ((alphabet[:step])) #returns the letters of the alphabet from the step to the end, then adds the letters from the begnning to the step. 
-    joined_shifted_alphabets = ''.join(tuple(map(shift, alphabets)))
-    joined_alphabets = ''.join(alphabets)
+    joined_shifted_alphabets = ''.join(tuple(map(shift, caes_alphabets)))
+    joined_alphabets = ''.join(caes_alphabets)
     table = str.maketrans(joined_alphabets, joined_shifted_alphabets) #creates a translation guide between the entire alphabet and the caesar shifted ones
     return summary.translate(table) #uses this guide to translate and return the summary. 
+print(caesar("uh i didn't break anything right"))
+
+def piglatin(summary): # i know pig latin isn't a cipher but shush. currently it does not piglatinify stuff in brackets. 
+    mus = summary.split() # to fix: work with punctuation properly
+    trans = []
+    for word in mus: 
+        if word[0] not in vowels and word[0] in (lowercase + uppercase) and word[-1] in (lowercase+uppercase):
+            trans.append(word[1:]+(word[0]).lower()+"ay")
+        else:
+            trans.append(word)
+    return ' '.join(trans)
 
 # more to come - basic substitution cipher and mixed substituion cipher
+print(piglatin("(Brackets go brr). punctuation too!! sdjf "))
